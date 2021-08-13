2a6590c1a9395928723d023efbe2e700795460b1
Austin Baxley <ausbaxley@gmail.com>
2021-08-13 19:40:02 -0400

    added get route to retrieve workout types

--- models/index.js
+++ models/index.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  date: {
+  day: {
     type: Date,
     default: Date.now,
   },
   exercises: Array,
 });
 
+const Workout = mongoose.model("Workout", workoutSchema);
 const db = { Workout: Workout };
 
 module.exports = db;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const { Router } = require("express");
+const { Workout } = require("../models");
 const db = require("../models");
+
+router.get("/", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .then((dbWorkout) => {
+      res.send(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
