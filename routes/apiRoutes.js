53bea98c41eac125e4c2bbbafee890fdfed4ed45
Austin Baxley <ausbaxley@gmail.com>
2021-08-13 20:17:52 -0400

    added /range route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,19 @@ const { Router } = require("express");
 const { Workout } = require("../models");
 const db = require("../models");
 
+// get routes
 router.get("/", (req, res) => {
+  Workout.find({})
+    .sort({ day: 1 })
+    .then((dbWorkout) => {
+      res.send(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/range", (req, res) => {
   Workout.find({})
     .sort({ day: -1 })
     .then((dbWorkout) => {
@@ -13,5 +25,16 @@ router.get("/", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+// post routes
+// router.post("/", ({ body }, res) => {
+//   Workout.insertMany(body)
+//     .then((dbWorkout) => {
+//       res.send(dbWorkout);
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
 
 module.exports = router;
