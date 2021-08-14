7887f1d089cc94d5fd51ccb5cc57dc3fb3fd9279
Austin Baxley <ausbaxley@gmail.com>
2021-08-13 21:28:43 -0400

    finished routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,11 @@ const db = require("../models");
 
 // get routes
 router.get("/", (req, res) => {
-  Workout.find({})
+  Workout.aggregate([
+    {
+      $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+    },
+  ])
     .sort({ day: 1 })
     .then((dbWorkout) => {
       res.send(dbWorkout);
@@ -16,8 +20,24 @@ router.get("/", (req, res) => {
 });
 
 router.get("/range", (req, res) => {
-  Workout.find({})
-    .sort({ day: -1 })
+  Workout.aggregate([
+    {
+      $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// post route
+router.post("/", ({ body }, res) => {
+  Workout.create(body)
     .then((dbWorkout) => {
       res.send(dbWorkout);
     })
@@ -26,15 +46,16 @@ router.get("/range", (req, res) => {
     });
 });
 
-// post routes
-// router.post("/", ({ body }, res) => {
-//   Workout.insertMany(body)
-//     .then((dbWorkout) => {
-//       res.send(dbWorkout);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
+// put route
+router.put("/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => res.json(err));
+});
 
 module.exports = router;
